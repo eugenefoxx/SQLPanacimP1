854eead398982a61a228ecabff622a89fc80b2a3
eugenefoxx <eugenefrompechora@gmail.com>
2021-11-24 16:27:35 +0300

    add pcb_panel.sql

---
+++ common_query.sql
@@ -0,0 +1,16 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+-- посмотреть пару job_id и setup_id
+SELECT TOP 5000 [JOB_ID]
+      ,[SETUP_ID]
+FROM [PanaCIM].[dbo].[job_products]
+order by [SETUP_ID]
+
+-- посмотреть список ордеров по изделиям
+SELECT TOP 100000 [WORK_ORDER_ID]
+      ,[WORK_ORDER_NAME]
+      ,[LOT_SIZE]
+      ,[JOB_ID]
+      ,[MASTER_WORK_ORDER_ID]
+      ,[COMMENTS]
+FROM [PanaCIM].[dbo].[work_orders]
+order by [JOB_ID]
